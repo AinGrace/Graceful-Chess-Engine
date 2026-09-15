aacbbc1f53ae768e73fadebb46a5ce93917e7a2f
AinGrace <qnicat32@gmail.com>
2026-09-15 18:28:17 +0400

    tt quiesce

--- engine/src/search.rs
+++ engine/src/search.rs
@@ -218,20 +218,6 @@ where
     result
 }
 
-fn should_extend(prev_score: &Score, cur_score: &Score, best_move_changed: bool) -> f64 {
-    let mut factor = 1.0;
-
-    if *cur_score < *prev_score - 100 {
-        factor *= 1.3;
-    }
-
-    if best_move_changed {
-        factor *= 1.25;
-    }
-
-    factor
-}
-
 fn negamax(
     pos: &mut Position,
     depth: u8,
@@ -254,10 +240,6 @@ fn negamax(
     }
 
     let mut bound = Bound::Upper;
-
-    // if depth == 0 {
-    //     return NegamaxResult::new(eval::static_eval(pos), None, *nodes);
-    // }
 
     if depth == 0 {
         return quiesce(pos, 7, tt_opts, alpha, beta, stop_flag, time_control, nodes);
